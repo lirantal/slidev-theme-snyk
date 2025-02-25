763e614c0dfdf6a53c5b47fa45681b734fc80c32
Liran Tal <liran.tal@gmail.com>
2025-02-25 11:56:09 +0200

    chore: initial project scaffol with basic snyk color scheme in dark mode

---
+++ layouts/cover-center.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="slidev-layout cover-center flex flex-col items-center justify-center text-center">
+    <div class="my-auto w-full">
+      <slot />
+    </div>
+  </div>
+</template>
